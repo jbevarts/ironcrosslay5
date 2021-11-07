463050204caaf1665b21417ec258807acd1ce996
Jerry Evarts <jerryevarts@gmail.com>
2021-11-06 18:41:02 -0700

    add ability to press once

--- ironcrosslay5.go
+++ ironcrosslay5.go
@@ -8,12 +8,12 @@ import (
 
 const (
 	// not accounting for commission yet
-	bankroll         = 10000
+	bankroll         = 1000
 	numrolls         = 1000
-	numgames         = 10000
+	numgames         = 20000
 	lay5payment      = 0.67
 	place6or8payment = 1.17
-	lay5Bet          = 150
+	lay5Bet          = 75
 	placeBet         = 25
 	fieldBet         = 0
 	fieldhastriple   = true
@@ -66,6 +66,7 @@ type gameState struct {
 	place6value    int
 	place8value    int
 	fieldBetValue  int
+	shouldPress    bool
 }
 
 func createGameState() *gameState {
@@ -76,6 +77,7 @@ func createGameState() *gameState {
 		rollsThrown:    0,
 		playerBankroll: bankroll,
 		lay5value:      0,
+		shouldPress:    true,
 	}
 
 	return newGame
@@ -125,7 +127,6 @@ func (g *gameState) rollTheDice() int64 {
 
 func (g *gameState) processRoll(roll int) {
 	profit := 0
-
 	if g.pointOn {
 		switch roll {
 		case 2:
@@ -176,10 +177,17 @@ func (g *gameState) payTheField(triple bool) int {
 }
 
 func (g *gameState) payThe6() int {
-	payment := int(place6or8payment*float64(g.place6value)) + g.place6value - g.fieldBetValue
-	g.playerBankroll = g.playerBankroll + payment
-	g.place6value = 0
-	g.fieldBetValue = 0
+	payment := 0
+	if g.shouldPress && g.playerBankroll > g.place6value {
+		g.place6value = 2 * g.place6value
+		g.playerBankroll = g.playerBankroll - g.place6value
+		g.shouldPress = false
+	} else {
+		payment = int(place6or8payment*float64(g.place6value)) + g.place6value - g.fieldBetValue
+		g.playerBankroll = g.playerBankroll + payment
+		g.place6value = 0
+		g.fieldBetValue = 0
+	}
 
 	return payment
 }
@@ -193,10 +201,17 @@ func (g *gameState) clear5() int {
 }
 
 func (g *gameState) payThe8() int {
-	payment := int(place6or8payment*float64(g.place8value)) + g.place8value - g.fieldBetValue
-	g.playerBankroll = g.playerBankroll + payment
-	g.place8value = 0
-	g.fieldBetValue = 0
+	payment := 0
+	if g.shouldPress && g.playerBankroll > g.place6value {
+		g.place8value = 2 * g.place8value
+		g.playerBankroll = g.playerBankroll - g.place8value
+		g.shouldPress = false
+	} else {
+		payment := int(place6or8payment*float64(g.place8value)) + g.place8value - g.fieldBetValue
+		g.playerBankroll = g.playerBankroll + payment
+		g.place8value = 0
+		g.fieldBetValue = 0
+	}
 
 	return payment
 }
